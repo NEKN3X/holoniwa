142e38e79d2d91a98f6a408b4de28a3839d50999
nekn3x <nekn3x@gmail.com>
2022-04-20 14:29:56 +0900

    channels, videos

--- app/routes/channels/index.tsx
+++ app/routes/channels/index.tsx
@@ -1,16 +1,14 @@
 import { db } from "~/utils/db.server"
 import { json } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
-import type { Video } from "@prisma/client"
+import type { Channel } from "@prisma/client"
 import type { LoaderFunction } from "@remix-run/node"
 
-type LoaderData = { videos: Array<Video> }
+type LoaderData = { channels: Array<Channel> }
 
 export const loader: LoaderFunction = async () => {
   const data: LoaderData = {
-    videos: await db.video.findMany({
-      take: 5,
-    }),
+    channels: await db.channel.findMany(),
   }
 
   return json(data)
@@ -20,8 +18,8 @@ export default function JokesIndexRoute() {
   const data = useLoaderData<LoaderData>()
   return (
     <ul>
-      {data.videos.map((video) => (
-        <li key={video.id}>{video.title}</li>
+      {data.channels.map((channel) => (
+        <li key={channel.id}>{channel.title}</li>
       ))}
     </ul>
   )
