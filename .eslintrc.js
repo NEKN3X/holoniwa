50df7309bd455591c80c2bc7f4afef486b6ecd31
nekn3x <nekn3x@gmail.com>
2022-04-23 22:04:19 +0900

    deploy succesfly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
-{
-  "extends": [
+/**
+ * @type {import('@types/eslint').Linter.BaseConfig}
+ */
+module.exports = {
+  extends: [
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
     "prettier",
-    "plugin:import/errors"
+    "plugin:import/errors",
   ],
-  "plugins": [
-    "@typescript-eslint",
-    "import"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "env": {
-    "node": true,
-    "es6": true
+  plugins: ["@typescript-eslint", "import"],
+  parser: "@typescript-eslint/parser",
+  env: {
+    node: true,
+    es6: true,
   },
-  "rules": {
+  rules: {
     "no-unused-vars": 0,
     "@typescript-eslint/no-unused-vars": 1,
     "sort-imports": 0, // ESLint標準のsort-importsは、import文のソート順を指定できないため、無効化
@@ -22,10 +22,10 @@
     "import/order": [
       2,
       {
-        "alphabetize": {
-          "order": "asc"
+        alphabetize: {
+          order: "asc",
         },
-        "groups": [
+        groups: [
           "index",
           "sibling",
           "parent",
@@ -33,19 +33,16 @@
           "external",
           "builtin",
           "object",
-          "type"
-        ]
-      }
-    ]
+          "type",
+        ],
+      },
+    ],
   },
-  "settings": {
+  settings: {
     "import/resolver": {
-      "node": {
-        "extensions": [
-          ".ts",
-          ".tsx"
-        ]
-      }
-    }
-  }
+      node: {
+        extensions: [".ts", ".tsx"],
+      },
+    },
+  },
 }
